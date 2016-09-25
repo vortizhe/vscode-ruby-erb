6f99cbe2a106f56d318773db1fc5bfee16edd981
Victor Ortiz <kespers@gmail.com>
2016-09-25 20:26:55 +0200

    Improve selection

--- erb.js
+++ erb.js
@@ -28,22 +28,22 @@ function deactivate() {
 }
 exports.deactivate = deactivate;
 
-function findSorroundingBlock(text) {
+function findSorroundingTags(text) {
   return [text.match(erb_opener_regex)[0], text.match(erb_closer_regex)[0]];
 }
 
-function insertErbBlock(text) {
+function insertErbTags(text) {
   return `${erb_blocks[0][0]} ${text} ${erb_blocks[0][1]}`;
 }
 
 
-function replaceErbBlock(text) {
-  let tags = findSorroundingBlock(text);
-  let next_tags = getNextErbBlock(tags);
+function replaceErbTags(text) {
+  let tags = findSorroundingTags(text);
+  let next_tags = getNextErbTags(tags);
   return text.replace(tags[0], next_tags[0]).replace(tags[1], next_tags[1]);
 }
 
-function getNextErbBlock(tags) {
+function getNextErbTags(tags) {
   let tags_str = JSON.stringify(tags);
   for (let i = 0; i < erb_blocks.length; i++) {
     if (JSON.stringify(erb_blocks[i]) == tags_str) {
@@ -58,27 +58,47 @@ function getNextErbBlock(tags) {
   return erb_blocks[0]
 }
 
+function getSelectionRange(selection, editor) {
+  let line = editor.document.lineAt(selection.start);
+  let selectedText = editor.document.getText(selection);
+
+  if ( selection.isEmpty && editor.document.getText(selection).trim().length == 0 && !line.isEmptyOrWhitespace) {
+    let start = new vscode.Position(selection.start.line, line.firstNonWhitespaceCharacterIndex);
+    let end = line.range.end;
+    let newSelection = new vscode.Selection(start, end);
+    editor.document.getText(newSelection)
+    return new vscode.Range(start, end);
+  }
+  return new vscode.Range(selection.start, selection.end);
+}
+
 function toggleTags(editor) {
   let selectionsMap = {};
-  let selections = editor.selections.filter(function (selection) { return selection.isSingleLine && !selection.isEmpty; });
-  selections.forEach(function (selection) { return selectionsMap[selection.start.line] ?
-    selectionsMap[selection.start.line].push(selection) :
-    selectionsMap[selection.start.line] = [selection]; });
   let newSelections = [];
   let lineOffset = 0;
+  let selections = editor.selections.filter(function(selection) { return selection.isSingleLine });
+
+  // Push selections to selectionsMap grouped by line
+  selections.forEach(function (selection) {
+    return selectionsMap[selection.start.line] ? selectionsMap[selection.start.line].push(selection) : selectionsMap[selection.start.line] = [selection];
+  });
+
+
   editor.edit(function(editBuilder) {
     for (let key in selectionsMap) {
       if (selectionsMap.hasOwnProperty(key)) {
         lineOffset = 0;
+        // Order selections by ltr position
         selectionsMap[key] = selectionsMap[key].sort(function (first, second) { return first.end.isBefore(second.start) ? -1 : 1; });
-        selectionsMap[key].forEach(function (selection) {
-          let selectedRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+
+        selectionsMap[key].forEach(function(selection) {
+          let selectedRange = getSelectionRange(selection, editor);
           let selectedText = editor.document.getText(selectedRange);
-          let newText = '';
+          let newText;
           if (selectedText.match(erb_regex)) {
-            newText = replaceErbBlock(selectedText);
+            newText = replaceErbTags(selectedText);
           } else {
-            newText = insertErbBlock(selectedText);
+            newText = insertErbTags(selectedText);
           }
           let delta = newText.length - selectedText.length;
           newSelections.push(new vscode.Selection(selection.start.line, selection.start.character + lineOffset, selection.end.line, selection.end.character + lineOffset + delta));
